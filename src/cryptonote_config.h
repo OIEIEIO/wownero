8263658cc74ec163cb10b19ab43ea70cdbc91130
wowario <wowario@protonmail.com>
2019-02-08 11:50:15 +0300

    change ports, network id

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -182,11 +182,11 @@ namespace config
     uint64_t const CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX = 53;
     uint64_t const CRYPTONOTE_PUBLIC_INTEGRATED_ADDRESS_BASE58_PREFIX = 54;
     uint64_t const CRYPTONOTE_PUBLIC_SUBADDRESS_BASE58_PREFIX = 63;
-    uint16_t const P2P_DEFAULT_PORT = 11180;
-    uint16_t const RPC_DEFAULT_PORT = 11181;
-    uint16_t const ZMQ_RPC_DEFAULT_PORT = 11182;
+    uint16_t const P2P_DEFAULT_PORT = 12180;
+    uint16_t const RPC_DEFAULT_PORT = 12181;
+    uint16_t const ZMQ_RPC_DEFAULT_PORT = 12182;
     boost::uuids::uuid const NETWORK_ID = { {
-        0x11, 0x33, 0xFF, 0x77 , 0x61, 0x04 , 0x41, 0x61, 0x17, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x11
+        0x11, 0x33, 0xFF, 0x77 , 0x61, 0x04 , 0x41, 0x61, 0x17, 0x74, 0x03, 0x14, 0x45, 0xA2, 0x44, 0x21
       } }; // Bender's daydream
     std::string const GENESIS_TX = "013c01ff0001ffffffffff1f029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd088071210160eb755f618a2336055dee60f307fe0ded81c5b37b53d310175ca9ee69b0c8ad";
     uint32_t const GENESIS_NONCE = 70;
